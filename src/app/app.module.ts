f1d733901f3f45356b51233d1a70d5d560091742
djo <djo@mail.com>
2018-09-15 21:02:33 +0100

    Init structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login' , component: LoginComponent },
+  { path: 'dashboard' , component: DashboardComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
